265e3bf19247a1e26b01c51362d6b48b726d5f83
Weronika <weranaj@gmail.com>
2022-04-03 16:57:18 +0200

    v.0.2.3

--- app/src/main/java/com/example/gamezone/LoginActivity.java
+++ app/src/main/java/com/example/gamezone/LoginActivity.java
@@ -1,4 +1,38 @@
 package com.example.gamezone;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+}
+
+    public void onButLoginClicked (View view){
+
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("123")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else Toast.makeText(this, "Incorrect login or password", Toast.LENGTH_LONG).show();
+        
+    }
+
+    public void finishLoginActivity (View view){ finish(); }
+
+
 }
--- app/src/main/java/com/example/gamezone/ProfileActivity.java
+++ app/src/main/java/com/example/gamezone/ProfileActivity.java
@@ -1,4 +1,20 @@
 package com.example.gamezone;
 
-public class ProfileActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.gamezone.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        TextView tvName = findViewById(R.id.groups_container);
+
+    }
 }
